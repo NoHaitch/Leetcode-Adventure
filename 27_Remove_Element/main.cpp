c7bdf3b604c7b52c5b94554603d847b828635eaf
Chiko but no H <franciscotrianto@gmail.com>
2023-12-22 23:46:29 +0700

    feat: problem 27

---
+++ 27_Remove_Element/main.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int j = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != val){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+};

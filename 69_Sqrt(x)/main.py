cea439c3a84160e23477226ad2cca8492968e204
Chiko but no H <franciscotrianto@gmail.com>
2024-01-04 23:53:28 +0700

    feat: problem 69 - easy

---
+++ 69_Sqrt(x)/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        low, high = 0, x
+        while low <= high :
+            mid = (low + high) // 2
+            if mid * mid == x :
+                return mid
+            elif mid * mid > x :
+                high = mid - 1
+            else :
+                low = mid + 1
+        return high
